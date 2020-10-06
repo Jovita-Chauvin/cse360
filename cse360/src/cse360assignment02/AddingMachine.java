33e8c5fb60589f61ea32b16ed1c2a0f93a53ddcf
Jovita Chauvin <chauvin.jovita@gmail.com>
2020-10-05 18:49:42 -0700

    initial version commit - Assignment2

---
+++ cse360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,62 @@
+/*
+ * Jovita Chauvin
+ * October 5, 2020
+ * CSE360 - Assignment 2
+ */
+
+package cse360assignment02;
+
+public class AddingMachine
+{
+	private int total;
+	
+	/**
+	 * Constructor
+	 */
+	public AddingMachine() 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+  
+	/**
+	 * @return the current total
+	 */
+	public int getTotal()
+	{
+		return 0;
+	}
+  
+	/**
+	 * 
+	 * @param value, adds value to total variable
+	 */
+	public void add (int value)
+	{
+	
+	}
+		
+	/**
+	 * 
+	 * @param value, subtracts value to total variable
+	 */
+	public void subtract (int value)
+	{
+	
+	}
+
+	/**
+	 * @return string with history of the transactions
+	 */
+	public String toString () 
+	{
+		return "";
+	}
+
+	/**
+	 * clears the history
+	 */
+	public void clear() 
+	{
+		
+	}
+}
