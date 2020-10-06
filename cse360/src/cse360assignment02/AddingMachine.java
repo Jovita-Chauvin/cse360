9b3422028cdd5ce0febc0c74fbe47607ca1748aa
Jovita Chauvin <chauvin.jovita@gmail.com>
2020-10-05 23:32:46 -0700

    second version commit - Assignment2

--- cse360/src/cse360assignment02/AddingMachine.java
+++ cse360/src/cse360assignment02/AddingMachine.java
@@ -9,54 +9,77 @@ package cse360assignment02;
 public class AddingMachine
 {
 	private int total;
+	StringBuffer past = new StringBuffer();
 	
 	/**
 	 * Constructor
+	 * sets total to 0 
+	 * adds beginning of past string to 0 
 	 */
 	public AddingMachine() 
 	{
 		total = 0;  // not needed - included for clarity
+		past.append(0);
 	}
   
 	/**
+	 * method gets the current total
 	 * @return the current total
 	 */
 	public int getTotal()
 	{
-		return 0;
+		return total;
 	}
   
 	/**
-	 * 
-	 * @param value, adds value to total variable
+	 * method adds the a value to the total value
+	 * @param value  the integer added to total 
 	 */
 	public void add (int value)
 	{
-	
+		//appends the value and addition sign to end of "past" string
+		past.append(" " + "+" + " " + value);
+		
+		//adds value to total
+		total =  value + total;
 	}
 		
 	/**
-	 * 
-	 * @param value, subtracts value to total variable
+	 * method dubtracts value from total value
+	 * @param value  the integer subtracted from total
 	 */
 	public void subtract (int value)
 	{
-	
+		//appends the value and minus sign to end of "past" string
+		past.append(" " + "-" + " " + value);
+		
+		//subtracts value from total
+		total = value - total; 
 	}
 
 	/**
+	 * method returns a string that has been appended from previous methods
 	 * @return string with history of the transactions
 	 */
 	public String toString () 
 	{
-		return "";
+		//Initializes string
+		String record = " ";
+		
+		//gets appended string history 
+		record = past.toString();
+		
+		//returns the record of history
+		return record;
 	}
 
 	/**
-	 * clears the history
+	 * clears the "history" that was appended in previous methods
 	 */
 	public void clear() 
 	{
-		
+		//deletes the "past" string starting at index 1
+		//keeps starting zero
+		past.delete(1, past.length());
 	}
 }
